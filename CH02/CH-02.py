6fa31297d123446a3cdd42bcc7a0f32c56ec7b03
Lucky6 <35544184+ljw30432@users.noreply.github.com>
2020-02-27 11:33:22 +0800

    A1. build a origin mode perception model because did not find how to fix the out of memory issue when calculating the Gram_metrics under antithetical mode. 2。 修改了代码，重新设计了传统形式计算函数，因为无法解决在对偶形式下训练过程中，计算Gram_metrics 进程被杀掉的问题。 3. accuracy score is  0.9836219336219336

--- CH02/CH-02.py
+++ CH02/CH-02.py
@@ -11,38 +11,10 @@ from sklearn.metrics import accuracy_score
 pwd = os.path.dirname(os.path.abspath(__file__))
 save_path = os.path.join(pwd,'result.txt')
 Learning_rate = 0.0001                                 # 学习步长
-MAX_iter = 10000                                 # 学习次数
-feature_length = 324                                # hog特征维度
+MAX_iter = 10000  # hog特征维度
 classes = 2 #类别
 
 
-def get_hog_features(trainset):
-    """
-    版权声明：本文为CSDN博主「wds2006sdo」的原创文章，遵循
-    CC
-    4.0
-    BY - SA
-    版权协议，转载请附上原文出处链接及本声明。
-    原文链接：https: // blog.csdn.net / wds2006sdo / article / details / 51923546
-    :param trainset:
-    :return:
-    """
-    features = []
-
-    hog = cv2.HOGDescriptor('../hog.xml')
-
-    for img in trainset:
-        img = np.reshape(img, (28, 28))
-        cv_img = img.astype(np.uint8)
-
-        hog_feature = hog.compute(cv_img)
-        # hog_feature = np.transpose(hog_feature)
-        features.append(hog_feature)
-
-    features = np.array(features)
-    features = np.reshape(features, (-1, 324))
-
-    return features
 
 
 
@@ -52,7 +24,7 @@ class Perception(object):
         self.a = np.zeros(self.num_features).reshape(1,-1) # (1,num_features)
         self.b = np.zeros(1) # int
         self.learning_rate = learning_rate
-        self.w = np.zeros(self.num_features+1).reshape(1, -1)  # (1,num_features)
+        self.w = [0.0] * (num_features + 1)  # (1,num_features)
 
     def train(self, X, Y):
         """
@@ -70,12 +42,16 @@ class Perception(object):
         """
         correct_count = 0
         times = 0
-        MAX_ITER = 5000
+        MAX_ITER = 10
         num_samples = len(X)
         # 异常判断
         assert len(X) == len(Y)
         num_features = X.shape[1]
-        # for 循环得到矩阵
+        """
+        直接构建矩阵会导致内存不足，没办法运行
+        Process finished with exit code 137 (interrupted by signal 9: SIGKILL)
+        """
+
         t1 = time.time()
         gram_matrix_1 = np.zeros(num_samples * num_features * num_features).reshape(
             [num_samples, num_features, num_features])
@@ -85,10 +61,10 @@ class Perception(object):
             gram_matrix_1[i] = line.dot(line.transpose(1, 0))
         print("for loop gram_matrix use %s" % (str(time.time() - t1)))
         # 直接通过矩阵计算得到矩阵
-        # t2 = time.time()
-        # temp_X = X[:, :, np.newaxis]
-        # gram_matrix_2 = np.matmul(temp_X, temp_X.transpose(0, 2,1))
-        # print("for matrix_mul gram_matrix use %s" % (str(time.time() - t2)))
+        t2 = time.time()
+        temp_X = X[:, :, np.newaxis]
+        gram_matrix_2 = np.matmul(temp_X, temp_X.transpose(0, 2, 1))
+        print("for matrix_mul gram_matrix use %s" % (str(time.time() - t2)))
 
         # 由书本公式证明，只要数据集线性可分，那么就一定能在有限次数内找到超平面
         """
@@ -96,8 +72,8 @@ class Perception(object):
         """
         while times <= MAX_ITER and correct_count <= MAX_ITER:
             index = random.choice(range(len(X)))
-            line = gram_matrix_1[index] # (num_features,num_features)
-            y = Y[index]
+            line = gram_matrix_2[index]  # (num_features,num_features)
+            y = Y[index] * 2 - 1
             if ((y * np.sum(np.dot(self.a,line))) ) * y < 0:
                 self.a += self.a + self.learning_rate
                 self.b += self.b + self.learning_rate*y
@@ -106,10 +82,7 @@ class Perception(object):
             times += 1
         print("train completed, iterations= %d" %times)
 
-    def train_orig(self, X, Y):
-        pass
-
-    def predict_one(self,x):
+    def predict_one(self, x):
         """
 
         :param new_x: numpy.array(1,num_features)
@@ -121,6 +94,40 @@ class Perception(object):
     def predict(self, X):
         res = [(np.dot(self.a, i.transpose(1, 0)) + self.b) for i in X]
         return res
+
+
+    def train_orig(self, X, Y):
+        correct_count = 0
+        times = 0
+        MAX_ITER = 5000
+        num_samples = len(X)
+        # 异常判断
+        assert len(X) == len(Y)
+        num_features = X.shape[1]
+        # print(type(X),type(Y),num_features,num_samples) #<class 'numpy.ndarray'> <class 'numpy.ndarray'> 784 28140
+        while times <= MAX_ITER and correct_count <= MAX_ITER:
+            index = random.choice(range(0, num_samples - 1))
+            y = (Y[index] * 2) - 1
+            line = list(X[index])
+            line.append(1.0)
+            res = sum([line[i] * self.w[i] for i in range(len(self.w))]) * y
+            if res > 0:
+                correct_count += 1
+            else:
+                for i in range(len(self.w)):
+                    self.w[i] += self.learning_rate * (y * line[i])
+
+    def pred_org_one(self, x):
+        # list 操作和 append 操作必须要分开
+        x = list(x)
+        x.append(1.0)
+        res = sum([x[i] * self.w[i] for i in range(len(self.w))])
+        return int(res > 0)
+
+    def pred_org(self, X):
+        pred_list = [self.pred_org_one(i) for i in X]
+        return pred_list
+
 
 
 
@@ -135,24 +142,24 @@ if __name__ == '__main__':
     """
     先对函数进行测试
     """
-    print("start and initialize the Percetion")
-    perception = Perception(num_features=feature_length,learning_rate=Learning_rate)
     t1 = time.time()
     print("load data")
     raw_data = pd.read_csv(os.path.join(pwd,'data/train_binary.csv'), header=0)
     data = raw_data.values
     imgs = data[0::, 1::]
     labels = data[::, 0]
-    features = get_hog_features(imgs)
-    train_features, test_features, train_labels, test_labels = train_test_split(features, labels, test_size=0.33,random_state=1)
+    train_features, test_features, train_labels, test_labels = train_test_split(imgs, labels, test_size=0.33,
+                                                                                random_state=1)
     print(" use %s to load the data"%(str(time.time()-t1)))
+    print("start and initialize the Percetion")
+    perception = Perception(num_features=train_features.shape[1], learning_rate=Learning_rate)
     print("start training ")
     t2 = time.time()
-    perception.train(X=train_features,Y=train_labels)
+    perception.train_orig(X=train_features, Y=train_labels)
     print("cost ",t2-time.time(),"s to train")
     print("start test")
-    res = perception.predict(test_features)
-    acc = accuracy_score(res,test_features)
+    res = perception.pred_org(test_features)
+    acc = accuracy_score(res, test_labels)
     print("accuracy score is ",acc)
     """
     测试用例
